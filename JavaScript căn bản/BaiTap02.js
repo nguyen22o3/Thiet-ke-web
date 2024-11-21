a7de3c3cb7e038c21b38cc6a5e1df103418c491c
nguyen22o3 <133405406+nguyen22o3@users.noreply.github.com>
2024-11-21 08:34:02 +0700

    ...

--- JavaScript căn bản/BaiTap02.js
+++
@@ -1,5 +0,0 @@
-// Define a function named print_current_page
-function print_current_page() {
-    // Call the window.print() method to initiate the printing of the current page
-    window.print();
-} 
