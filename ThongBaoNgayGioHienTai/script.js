340d6de0f00f512ad549b04f8601aa23feb77a67
nguyen22o3 <133405406+nguyen22o3@users.noreply.github.com>
2024-11-21 09:46:10 +0700

    Add files via upload

---
+++ ThongBaoNgayGioHienTai/script.js
@@ -0,0 +1,36 @@
+function showDateTime() {
+    
+    var today = new Date();
+    var day = today.getDay();
+    var daylist = ["Chủ nhật", "Thứ hai", "Thứ ba", "Thứ tư", "Thứ năm", "Thứ sáu", "Thứ bảy"];
+
+    var hour = today.getHours();
+    var minute = today.getMinutes();
+    var second = today.getSeconds();
+
+    var prepand = (hour >= 12) ? "PM" : "AM";
+    hour = (hour >= 12) ? hour - 12 : hour;
+
+    if (hour === 0 && prepand === 'PM') {
+        if (minute === 0 && second === 0) {
+            hour = 12;
+            prepand = 'Noon';
+        } else {
+            hour = 12;
+            prepand = 'PM';
+        }
+    }
+
+    if (hour === 0 && prepand === 'AM') {
+        if (minute === 0 && second === 0) {
+            hour = 12;
+            prepand = 'Midnight';
+        } else {
+            hour = 12;
+            prepand = 'AM';
+        }
+    }
+
+    var timeString = "Hôm nay là: " + daylist[day] + " và thời gian hiện tại là: " + hour + prepand + ":" + formatTime(minute) + ":" + formatTime(second);
+    document.getElementById("time").innerText = timeString;
+}
